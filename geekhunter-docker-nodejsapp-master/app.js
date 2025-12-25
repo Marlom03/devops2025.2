6b5eae4d6bc8347ee07f76572b519c4a9cdb2c85
Marlom03 <marlomrafael03@gmail.com>
2025-12-25 15:09:38 -0300

    Remove pasta geekhunter-docker-nodejsapp-master (mantém apenas workflows na raiz)

--- geekhunter-docker-nodejsapp-master/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', function (req, res) {
-    res.send('Hello word!');
-});
-app.listen(3000, function () {
-    console.log('Servidor na porta 3000!');
-});
